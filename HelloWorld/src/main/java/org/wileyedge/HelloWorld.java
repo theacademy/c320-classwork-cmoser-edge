036224f6fd526750710209ff1e1a969397531cf0
Calvin Moser <cmoser@thesoftwareguild.com>
2023-04-25 11:53:51 -0400

    Modify Hello World program to not ask question

--- HelloWorld/src/main/java/org/wileyedge/HelloWorld.java
+++ HelloWorld/src/main/java/org/wileyedge/HelloWorld.java
@@ -2,7 +2,7 @@ package org.wileyedge;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World, How are you?");
+        System.out.println("Hello World?");
     }
 
 }
