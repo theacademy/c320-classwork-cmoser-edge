1cf561ef7a8e96db9d6a3c5611b570bc1c73ce96
Calvin Moser <cmoser@thesoftwareguild.com>
2023-04-26 12:08:13 -0400

    Refactor code into methods

--- WindowMaster/src/main/java/com/wileyedge/windowmaster/WindowMaster.java
+++ WindowMaster/src/main/java/com/wileyedge/windowmaster/WindowMaster.java
@@ -4,6 +4,8 @@ package com.wileyedge.windowmaster;
 import java.util.Scanner;
 
 public class WindowMaster {
+    static Scanner myScanner;
+
     public static void main (String[] args) {
         float height;
         float width;
@@ -11,15 +13,15 @@ public class WindowMaster {
         String stringHeight;
         String stringWidth;
 
-        Scanner myScanner = new Scanner(System.in);
+        myScanner = new Scanner(System.in);
 
         // Ask user for information
-        System.out.println("Please enter window height:");
-        stringHeight = myScanner.nextLine();
+        height = retrieveHeight();
+
+
         System.out.println("Please enter window width:");
         stringWidth = myScanner.nextLine();
 
-        height = Float.parseFloat(stringHeight);
         width = Float.parseFloat(stringWidth);
         
         // calculate area and perimeter
@@ -30,10 +32,22 @@ public class WindowMaster {
         float cost = (3.5f * area) + (2.25f * perimeter);
 
         // display results
-        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window height = " + height);
         System.out.println("Window width = " + stringWidth);
         System.out.println("Window area = " + area);
         System.out.println("Window perimeter = " + perimeter);
+        displayCost(cost);
+        //System.out.println("Window cost = " + cost);
+    }
+
+    private static float retrieveHeight() {
+        System.out.println("Please enter window height:");
+        String stringHeight = myScanner.nextLine();
+        float height = Float.parseFloat(stringHeight);
+        return height;
+    }
+
+    private static void displayCost(float cost) {
         System.out.println("Window cost = " + cost);
     }
 }
