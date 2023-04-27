e8fbc2e253414674f13c3ecc0dd43731f832e110
Calvin Moser <cmoser@thesoftwareguild.com>
2023-04-27 11:52:29 -0400

    Add and use Window class

---
+++ WindowMaster/src/main/java/com/wileyedge/windowmaster/Window.java
@@ -0,0 +1,60 @@
+package com.wileyedge.windowmaster;
+
+public class Window {
+    // access modifier
+      // variable, field, attribute, property
+    private float height;
+    private float width;
+
+    private static int windowCount = 0;
+
+    // constructor
+    // Method overloading
+    public Window(float height, float width){
+        windowCount++;
+        this.height = height;
+        this.width = width;
+    }
+
+    // Default constructor
+    public Window() {
+        windowCount++;
+    }
+
+    public float generateCost() {
+        float area = height * width;
+        float perimeter = 2 * (height + width);
+
+        float cost = (3.5f * area) + (2.25f * perimeter);
+
+        return cost;
+    }
+
+    public static void showHelloWorld(){
+        System.out.println("Count: " + windowCount);
+        System.out.println("Hello World");
+    }
+
+    public void setHeight(float height) {
+        if (height < 0)
+            this.height = 0;
+        else
+            this.height = height;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public static int getWindowCount() {
+        return windowCount;
+    }
+}
--- WindowMaster/src/main/java/com/wileyedge/windowmaster/WindowMaster.java
+++ WindowMaster/src/main/java/com/wileyedge/windowmaster/WindowMaster.java
@@ -1,42 +1,64 @@
 package com.wileyedge.windowmaster;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WindowMaster {
     static Scanner myScanner;
 
     public static void main (String[] args) {
-        float height;
-        float width;
 
-        String stringHeight;
-        String stringWidth;
+        List<Window> windows = new ArrayList<>();
 
-        myScanner = new Scanner(System.in);
+        // Declare, initialize, and instantiate Window object
+        windows.add(new Window(3, 2.3f));
+        windows.add(new Window(20, 10));
+        windows.add(new Window());
+        windows.add(new Window());
+        windows.add(new Window());
 
-        // Ask user for information
-        height = retrieveHeight();
+
+        for (Window window: windows) {
+            System.out.println("height: " + window.getHeight());
+            System.out.println("width: " + window.getWidth());
+            System.out.println("cost: " + window.generateCost());
+        }
+
+        System.out.println("Count: " + Window.getWindowCount());
 
 
-        System.out.println("Please enter window width:");
-        stringWidth = myScanner.nextLine();
-
-        width = Float.parseFloat(stringWidth);
-        
-        // calculate area and perimeter
-        float area = height * width;
-        float perimeter = 2 * (height + width);
-
-        // calculate the cost
-        float cost = (3.5f * area) + (2.25f * perimeter);
-
-        // display results
-        System.out.println("Window height = " + height);
-        System.out.println("Window width = " + stringWidth);
-        System.out.println("Window area = " + area);
-        System.out.println("Window perimeter = " + perimeter);
-        displayCost(cost);
+//        float height;
+//        float width;
+//
+//        String stringHeight;
+//        String stringWidth;
+//
+//        myScanner = new Scanner(System.in);
+//
+//        // Ask user for information
+//        height = retrieveHeight();
+//
+//
+//        System.out.println("Please enter window width:");
+//        stringWidth = myScanner.nextLine();
+//
+//        width = Float.parseFloat(stringWidth);
+//
+//        // calculate area and perimeter
+//        float area = height * width;
+//        float perimeter = 2 * (height + width);
+//
+//        // calculate the cost
+//        float cost = (3.5f * area) + (2.25f * perimeter);
+//
+//        // display results
+//        System.out.println("Window height = " + height);
+//        System.out.println("Window width = " + stringWidth);
+//        System.out.println("Window area = " + area);
+//        System.out.println("Window perimeter = " + perimeter);
+//        displayCost(cost);
         //System.out.println("Window cost = " + cost);
     }
 
